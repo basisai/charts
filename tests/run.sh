258ade7c653b4c92505c36f56c01ff4c18bf7d8e
Chris Sng <chrissng@users.noreply.github.com>
2020-04-23 13:24:35 +0900

    cert-manager-aio - Update to cert-manager 0.14.2 chart (#27)
    * cert-manager-aio - Update to cert-manager 0.14.2 chart
    
    * bump chart version
    
    * fix linting issues and update chart testing for helm3
    
    * psp enable by default
    
    * fix travis settings

--- tests/run.sh
+++ tests/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-TEST_IMAGE="${TEST_IMAGE:-quay.io/helmpack/chart-testing:v2.3.3}"
+TEST_IMAGE="${TEST_IMAGE:-quay.io/helmpack/chart-testing:v3.0.0-rc.1}"
 
 docker run --rm -v "$(pwd):/charts" --workdir "/charts" \
     "${TEST_IMAGE}" \
