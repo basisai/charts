0dd58f90c75eda187168830d465bc83d5059055f
Yong Wen Chua <lawliet89@users.noreply.github.com>
2020-10-23 10:07:27 +0800

    Move charts around

--- tests/test.sh
+++ tests/test.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 BATS_IMAGE="${BATS_IMAGE:-basisai/bats-helm:v1.2.1}"
 
 # consul-esm
-docker run --rm -v "$(git rev-parse --show-toplevel)/consul-esm:/tests" --workdir "/tests" \
+docker run --rm -v "$(git rev-parse --show-toplevel)/charts/consul-esm:/tests" --workdir "/tests" \
     "${BATS_IMAGE}" \
     bats tests/unit
